948137c3452aa2c10d912d87820a6ac5d8aab765
Laura-Mediavilla-Birt <lmediavilla@ikaslea.birt.eus>
2026-09-20 10:21:18 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'DI01_TA06_Ejercicios',
+  webDir: 'www'
+};
+
+export default config;
